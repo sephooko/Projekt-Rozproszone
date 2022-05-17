f4263061d6885871eabb42d26fb0ec2d4d0b61e6
Malwina <yellowindmills@gmail.com>
2022-05-17 02:13:03 +0200

    sleep

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,13 +1,29 @@
 package server;
-
 import java.net.*;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.*;
+
+
+
+class users {
+    String Name;
+    String Surname;
+    long PESEL;
+    int AccNum;
+    float Balance;
+    
+/*    users(String Name, String Surname, long PESEL, int AccNum, float Balance) {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.PESEL = PESEL;
+        this.AccNum = AccNum;
+        this.Balance = Balance;
+    } */
+}
 
 public class Server {
 
@@ -49,222 +65,239 @@ public class Server {
             }
             System.out.println("Zakończona inicjalizacja strumieni...");
             System.out.println("Rozpoczęcie pętli głównej...");
-            List mainList = new ArrayList<>();
-            File usersFile = new File("users.txt");
-            String filePath = "C:\\Users\\yello\\OneDrive\\Pulpit\\Projekt-szko-a-master\\Server\\src\\server\\users.txt";
-            FileWriter fileWriter = new FileWriter(usersFile);
-            BufferedReader fileReader = null;
-            Scanner fileScan = new Scanner(usersFile);
-            while(fileScan.hasNext()){
-                Dane tmp = new Dane();
-                tmp.accNum = fileScan.nextInt();
-                tmp.imie = fileScan.next();
-                tmp.nazwisko = fileScan.next();
-                tmp.pesel = fileScan.nextLong();
-                tmp.money = fileScan.nextInt();
-                mainList.add(tmp);
-            }
             while (true) {
+
                 String login = "Podaj swój login";
                 out.writeBytes(login + "\n\r");
                 out.flush();
                 login = brinp.readLine();
-                try{
-                    fileReader = new BufferedReader(new FileReader(filePath));
-                } finally {
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-            }
-                if ("Admin".equals(login)) {
-                    String accNum = "Witaj w Terminalu Bankiera. Podaj numer konta, które chcesz modyfikować lub dodać.";
-                    out.writeBytes(accNum + "\n");
-                    out.flush();
 
-                    BufferedReader br=new BufferedReader(new FileReader(filePath));
-                    String st = br.readLine();
-                    while(st.length()==0 && st!=null){
-                        st = br.readLine();
-                    }
-                    if (accNum.equals(st)){
-                        String accExist = "Konto istnieje.Wybierz:\n1.Aby zmienić imie. 2.Aby zmienic nazwisko. 3.Aby zmienic pesel.";
-                        out.writeBytes(accExist + "\n");
-                        out.flush();
-                        if("1".equals(accExist)){
-                            String oldName = "Wprowadx stare imie:";
-                            out.writeBytes(oldName + "\n");
-                            out.flush();
-                            for (int i = 0; i < mainList.size(); i++) {
-                                if (mainList.get(i).equals(oldName)) {
-                                    String newName = "Wprowadz nowe imie:";
-                                    out.writeBytes(oldName + "\n");
+
+                ArrayList<users> users = null;
+                if (login == null) {
+                    String accnum = "Podaj nr.konta";
+                    out.writeBytes(accnum + '\n');
+                    out.flush();
+                    accnum = brinp.readLine();
+
+                    String tast = "Co chcesz zrobic: 1. Sprawdzić stan konta 2. Wypłacić środki 3. Wpłacić środki  4. zrobić przelew ";
+                    out.writeBytes(tast + "\n\r");
+                    out.flush();
+                    tast = brinp.readLine();
+
+                    Scanner scanner = new Scanner(new File("users.txt"));
+                    int line = 1;
+
+                    users = new ArrayList<>();
+
+                    while (scanner.hasNext()) {
+
+                        users user = new users();
+
+                        user.Name = scanner.next();
+                        user.Surname = scanner.next();
+                        user.PESEL = scanner.nextLong();
+                        user.AccNum = scanner.nextInt();
+                        user.Balance = scanner.nextFloat();
+
+                        users.add(user);
+
+/*                while(scanner.hasNext()) {
+
+                    users user = new users();
+
+                    user.Name = scanner.next();
+                    user.Surname = scanner.next();
+                    user.PESEL = scanner.nextLong();
+                    user.AccNum = scanner.nextInt();
+                    user.Balance = scanner.nextFloat();
+
+                    users.add(user);
+                } */
+
+
+//                users user = new users();
+//                user.Name = "Mateusz";
+//                user.Surname = "Sierski";
+//                user.PESEL = 002202200;
+//                user.AccNum = 8888;
+//                user.Balance = 2000;
+//
+//                users.add(user);
+
+
+                        int AccNumb = Integer.parseInt(accnum);
+
+
+                        if ("1".equals(tast)) {
+
+                            for (int i = 0; i < users.size(); i++) {
+
+                                if (users.get(i).AccNum == AccNumb) {
+                                    System.out.println("Stan konta " + users.get(i).Balance);
+                                    String money = "Stan konta " + users.get(i).Balance;
+                                    out.writeBytes(money + "\n\r");
                                     out.flush();
-                                    mainList.set(i, newName);
+                                    break;
+
+                                }
+                            }
+                        } else if ("2".equals(tast)) {
+
+                            String moneyT = "Ile pieniędzy wypłacić?";
+                            out.writeBytes(moneyT + '\n');
+                            out.flush();
+                            moneyT = brinp.readLine();
+
+                            int toWithdraw = Integer.parseInt(moneyT);
+
+                            for (int i = 0; i < users.size(); i++) {
+
+                                if (users.get(i).AccNum == AccNumb) {
+                                    float tmp = users.get(i).Balance;
+                                    tmp = tmp - toWithdraw;
+                                    users.get(i).Balance = tmp;
+                                    break;
+
+                                }
+                            }
+
+                        } else if ("3".equals(tast)) {
+
+                            String moneyF = "Ile pieniędzy wpłacić?";
+                            out.writeBytes(moneyF + '\n');
+                            out.flush();
+                            moneyF = brinp.readLine();
+
+                            int toDeposit = Integer.parseInt(moneyF);
+
+                            for (int i = 0; i < users.size(); i++) {
+
+                                if (users.get(i).AccNum == AccNumb) {
+                                    float tmp = users.get(i).Balance;
+                                    tmp = tmp + toDeposit;
+                                    users.get(i).Balance = tmp;
                                     break;
                                 }
                             }
-                            Files.write(Path.of(filePath), mainList, StandardCharsets.UTF_8);
-                        }
-                        else if ("2".equals(accExist)){
+
+
+                        } else if ("4".equals(tast)) {
+
+                            String przelew = "Ile pieniędzy przelać?";
+                            out.writeBytes(przelew + '\n');
+                            out.flush();
+                            przelew = brinp.readLine();
+                            String komu = "Ile pieniędzy przelać (nr.konta) ?";
+                            out.writeBytes(komu + '\n');
+                            out.flush();
+                            komu = brinp.readLine();
+
+                            int To = Integer.parseInt(komu);
+                            int transfer = Integer.parseInt(przelew);
+
+                            for (int i = 0; i < users.size(); i++) {
+
+                                if (users.get(i).AccNum == AccNumb) {
+                                    float tmp = users.get(i).Balance;
+                                    tmp = tmp - transfer;
+                                    users.get(i).Balance = tmp;
+                                } else if (users.get(i).AccNum == To) {
+                                    float tmp = users.get(i).Balance;
+                                    tmp = tmp + transfer;
+                                    users.get(i).Balance = tmp;
+                                }
+
+
+                            }
 
                         }
-                        else if ("3".equals(accExist)){
 
+                        FileOutputStream stream = new FileOutputStream("users.txt", false);
+                        PrintWriter save = new PrintWriter(stream);
+
+                        for (int i = 0; i < users.size(); i++) {
+                            save.print(users.get(i).Name + " ");
+                            save.print(users.get(i).Surname + " ");
+                            save.print(users.get(i).PESEL + " ");
+                            save.print(users.get(i).AccNum + " ");
+                            save.println(users.get(i).Balance + " ");
+                        }
+                        save.close();
+
+
+                        try {
+                            System.out.println("Odczytano linię: " + tast);
+                            if (tast == null || "quit".equals(tast)) {
+                                socket.close();
+                                System.out.println("Zakończenie pracy z klientem...");
+                                break;
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Błąd wejścia-wyjścia: " + e);
+                            break;
+                        }
+                    }
+                } else if ("Admin".equals(login)) {
+                    String insertAccN = "Witaj w Terminalu Bankiera. Podaj numer konta, które chcesz modyfikować lub dodać.";
+                    out.writeBytes(insertAccN + "\n");
+                    out.flush();
+                    brinp.readLine();
+
+                    BufferedReader br = new BufferedReader(new FileReader("users.txt"));
+                    String st = br.readLine();
+                    ArrayList<users> userList = new ArrayList<>(users);
+                    while (st.length() == 0) {
+                        st = br.readLine();
+                    }
+                    if (insertAccN.equals(st)) {
+                        String accExist = "Konto istnieje.Wybierz:\n1.Aby zmienić imie. 2.Aby zmienic nazwisko. 3.Aby zmienic pesel.";
+                        out.writeBytes(accExist + "\n");
+                        out.flush();
+                        if ("1".equals(accExist)) {
+//                            String oldName = "Wprowadx stare imie:";
+//                            out.writeBytes(oldName + "\n");
+//                            out.flush();
+                            String change = brinp.readLine();
+                            Scanner fileScan = new Scanner("users.txt");
+                            fileScan.hasNext();
+                            String dataSplit = fileScan.next();
+                            String[] result = dataSplit.split("");
+                            result[3].equals(change);
+
+                            String line = "Nowe imie: ";
+                            out.writeBytes(line + "\n\r");
+                            out.flush();
+                            int numberOfPara = 0;
+                            String save = brinp.readLine();
+                            userList.get(numberOfPara).Name = save.substring(0, 1).toUpperCase()+save.substring(1).toLowerCase();
+
+//                            Files.write(Path.of("users.txt"), (Iterable<? extends CharSequence>) users, StandardCharsets.UTF_8);
+                        } else if ("2".equals(accExist)) {
+                            String line = "Nowe nazwisko: ";
+                            out.writeBytes(line + "\n\r");
+                            out.flush();
+                            int numberOfPara = 0;
+                            String save = brinp.readLine();
+                            userList.get(numberOfPara).Surname = save.substring(0, 1).toUpperCase()+save.substring(1).toLowerCase();
+                        } else if ("3".equals(accExist)) {
+                            String line = "Nowy pesel: ";
+                            out.writeBytes(line + "\n\r");
+                            out.flush();
+                            int numberOfPara = 0;
+                            String save = brinp.readLine();
+                            userList.get(numberOfPara).PESEL = Long.parseLong(save);
                         }
                     } else {
                         String accNExist = "Konto nie istnieje. Czy chcesz dodać nowe? [yes/no]";
                         out.writeBytes(accNExist + "\n");
                         out.flush();
-                        if ("yes".equals(accNExist)){
-
-
-                        }
-                        else {
+                        if ("yes".equals(accNExist)) {
+                            
+                        } else {
                             break;
                         }
                     }
-                } else {
-                String accnum = "Podaj nr.konta";
-//                out.writeBytes(accnum + '\n');
-//                out.flush();
-//                accnum = brinp.readLine();
-
-                String tast = "Co chcesz zrobic: 1. Sprawdzić stan konta 2. Wypłacić środki 3. Wpłacić środki  4. zrobić przelew ";
-                out.writeBytes(tast + "\n\r");
-                out.flush();
-                tast = brinp.readLine();
-
-                FileReader fr = new FileReader(usersFile);
-                BufferedReader br = new BufferedReader(fr);
-                FileWriter fstream = new FileWriter("TempFile.txt", true);
-                BufferedWriter save = new BufferedWriter(fstream);
-                String moneyF = "Ile pieniędzy wpłacić?";
-
-                int tastReply = Integer.parseInt(tast);
-//                tastReply = brinp.read();
-//                out.flush();
-
-                int count = tastReply;
-                String s;
-                String[] words;
-                switch (count) {
-                    case 1: 
-                        while ((s=br.readLine())!=null) {
-                            words=s.split(" ");
-                            for (String word : words) {
-                                if (word.equals(accnum)){
-                                    System.out.println("Stan konta" + words[4]);
-                                }
-                            }
-                        }
-                        break;
-
-                    case 2:
-                        String moneyT = "Ile pieniędzy wypłacić?";
-                        out.writeBytes(moneyT + '\n');
-                        out.flush();
-                        moneyT = brinp.readLine();
-                        while ((s=br.readLine())!=null) {
-                            words=s.split(" ");
-                                if (words[3].equals(accnum)){
-                                    String tmp = words[4];
-                                    tmp = tmp + moneyT;
-                                    words[4] = tmp;
-                                }
-
-                            for (int i = 0; i < words.length; i++) {
-                                save.write(words[i] + " ");
-                            }   
-                            save.newLine();   
-                            }
-                        save.flush();
-                        save.close();
-                        break;
-
-                    case 3:
-
-
-                    out.writeBytes(moneyF + '\n');
-                    out.flush();
-                    moneyF = brinp.readLine();
-                    while ((s=br.readLine())!=null) {
-                        words=s.split(" ");
-                            if (words[3].equals(accnum)){
-                                String tmp = words[4];
-                                tmp = (tmp + moneyF);
-                                words[4] = tmp;
-                            }
-                        
-                        for (int i = 0; i < words.length; i++) {
-                            save.write(words[i] + " ");
-                            }   
-                        save.newLine();   
-                        }
-                        save.flush();
-                        save.close();
-                        break;
-
-
-                    case 4:
-
-                    String przelew = "Ile pieniędzy przelać?";
-                    out.writeBytes(przelew + '\n');
-                    out.flush();
-                    przelew = brinp.readLine();
-                    String komu = "Ile pieniędzy przelać (nr.konta) ?";
-                    out.writeBytes(komu + '\n');
-                    out.flush();
-                    komu = brinp.readLine();
-                    while ((s=br.readLine())!=null) {
-                        words=s.split(" ");
-                        if (words[3].equals(accnum)){
-                            String tmp = words[4];
-                            tmp = tmp + moneyF;
-                            words[4] = tmp;
-                        }
-                        else if (words[3].equals(komu)) {
-                            String tmp = words[4];
-                            tmp = tmp + moneyF;
-                            words[4] = tmp;
-                        }
-
-                    for (int i = 0; i < words.length; i++) {
-                        save.write(words[i] + " ");
-                        }
-                    }
-                    save.flush();
-                    save.close();
-                        break;
-
-                    default:
-                        String info = "Nie wybrano poprawnej operacji";
-                        out.writeBytes(info + '\n');
-                        out.flush();
-
-                    }     
-
-                    File old = new File("users.txt");
-                    old.delete();
-                    File New = new File("TempFile.txt");
-                    New.renameTo(old);
-
-                       
-                }                        
-                try {
-                    String line = brinp.readLine();
-                    System.out.println("Odczytano linię: " + line);
-                    if (line == null || "quit".equals(line)) {
-                        socket.close();
-                        System.out.println("Zakończenie pracy z klientem...");
-                        break;
-                    }
-                    out.writeBytes(line + "\n\r");
-                    System.out.println("Wysłano linię: " + line);
-                } catch (IOException e) {
-                    System.out.println("Błąd wejścia-wyjścia: " + e);
-                    break;
                 }
             }
-        }
-    }
-}
+    }}}
